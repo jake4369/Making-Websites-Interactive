f26ef49f225873dbc4d5ba457e3e50f3d02930d4
Jake Samuels <samuels242@googlemail.com>
2022-07-01 15:05:11 +0100

    JavaScript Challenge - Save to localStorage

---
+++ javascript-challenges-part-3/localStorage/index.js
@@ -0,0 +1,7 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+// localStorage.setItem("name", "Jake");
+// const name = localStorage.getItem("name");
+// console.log(name);
